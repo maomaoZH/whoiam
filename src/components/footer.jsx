dccfadf21ed04a3f6a2a13f45c2b7e0dffa491f8
maomao <maomao.zhang@immmr.com>
2018-09-06 14:49:59 +0200

    Updates

--- src/components/footer.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const Footer = () => {
-  return <div className="footer">All Rights Reserved © 2018</div>;
-};
-
-export default Footer;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
-import "font-awesome/css/font-awesome.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
